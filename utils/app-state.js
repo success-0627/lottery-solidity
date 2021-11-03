aa76033c0d5f925b94f466bde47c3acafe1f87f2
ZL910627 <zl910627@yahoo.com>
2021-11-03 23:06:25 +0800

    Fix

--- utils/app-state.js
+++ utils/app-state.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import React from "react";
 import _ from "lodash";
 
@@ -6,14 +7,8 @@ export const getSmartContract = (SC, web3, networkId, scAddr) => {
 	if (!networkId && !scAddr) {
 		return undefined;
 	}
-
 	if (networkId) {
-		const deployedNetwork = SC.networks[networkId];
-		return new web3.eth.Contract(
-			SC.abi,
-			deployedNetwork && deployedNetwork.address,
-		);
+		scAddr = deployedNetwork && deployedNetwork.address;
 	}
-
 	return new web3.eth.Contract(SC.abi, scAddr);
 };
